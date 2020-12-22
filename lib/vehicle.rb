38a74a179c9d1c4ee67e67f15472a4885eb4e06e
Keith Filkins <kafilkins@me.com>
2020-12-22 16:44:36 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,13 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+
+  def intialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
 
 end
